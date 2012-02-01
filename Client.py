58d27684e87a1f1915555219c0f57f359013ecca
Laurent BILLON <laurent.billonc@gmail.com>
2012-02-01 16:57:45 +0100

    Refactored code into right classes

--- Client.py
+++ Client.py
@@ -3,114 +3,96 @@ Created on Feb 1, 2012
 
 @author: lbillon
 '''
-from inspect import Traceback
-
-
-def __listItems():
-    print("Available Items :")
-    for i in __im.items:
-        print(__im.items[i].toString())
-
-def __parseInput(userInput):
-    if userInput == "/+":
-        __itemInput();
-
-    elif userInput == "/i":
-        __listItems();
-
-    elif userInput.startswith("/s "):
-        fileName = userInput.split(" ")[1]
-        try:
-            __im.saveToFile(fileName)
-            print("Saved item definitions to {}".format(fileName))
-        except IOError, err:
-            print err
-
-    elif userInput.startswith("/l "):
-        fileName = userInput.split(" ")[1]
-        try:
-            __im.loadFromFile(fileName)
-            print("Loaded item definitions from {}".format(fileName))
-            __listItems()
-        except IOError, err:
-            print err
-
-    elif userInput[0] == "/":
-        print "Command invalid"
-
-    else:
-        itemList = {}
-        sum = 0
-        paid = 0
-        for c in userInput :
-            try:
-                i = __im.items[c]
-                if i in itemList:
-                    itemList[i] = itemList[i] + 1
-                else:
-                    itemList[i] = 1
-            except KeyError:
-                print 'The item {} doesn\'t exist'.format(c)
-                return 1
-        print("Ordered Items: ")
-        for i in itemList:
-            print '{} x {} = {:.2f}'.format(itemList[i], i.name, itemList[i] * i.price)
-            sum += itemList[i] * i.price
-        print("Total:\n{:.2f}".format(sum))
-        while(True):
-                paid = raw_input("Amount given: ")
-
-
-                try:
-                    paid = float(paid)
-                    if(paid < 0):
-                        print("Transaction cancelled")
-                        break
-                    elif(paid < sum):
-                        print("Unsufficient amount : need {}".format(sum - paid))
-                    else:
-                        print("Anount to return : {:.2f}".format(paid - sum))
-                        print("Transaction committed\n")
-                        break
-                except :
-                    print("Invalid Value")
-                    continue
-
-
-
-
-
-
-
-
-def __itemInput():
-    print("Creating New Item")
-    try:
-        name = raw_input("Name? ")
-        identifier = raw_input("Identifier? ")
-        price = raw_input("Price? ")
-        i = Item.Item(name, identifier, price)
-        __im.items[identifier] = i
-    except Exception, err:
-        print(err)
-        print("Item not created")
-
-
-def __exitProgram():
-    print("terminating...")
-    sys.exit()
 
 if __name__ == '__main__':
     import sys
     import ItemManager, Item
 
+
     __im = ItemManager.ItemManager()
+
+    def __exitProgram():
+        print("terminating...")
+        sys.exit()
+
+    def __listItems():
+        print("Available Items :")
+        for i in __im.availableItems:
+            print(__im.availableItems[i].toString())
+
+    def __itemInput():
+        print("Creating New Item")
+        try:
+            name = raw_input("Name? ")
+            identifier = raw_input("Identifier? ")
+            price = raw_input("Price? ")
+            i = Item.Item(name, identifier, price)
+            __im.createNewItem(i)
+        except Exception, err:
+            print err
+            print("Item not created")
+
+
+    def __parseInput(userInput):
+        if userInput == "":
+            pass
+        elif userInput == "/+":
+            __itemInput();
+
+        elif userInput == "/i":
+            __listItems();
+
+        elif userInput.startswith("/s "):
+            fileName = userInput.split(" ")[1]
+            try:
+                __im.saveAvailableItemsToFile(fileName)
+                print("Saved item definitions to {}".format(fileName))
+            except IOError, err:
+                print err
+
+        elif userInput.startswith("/l "):
+            fileName = userInput.split(" ")[1]
+            try:
+                __im.loadAvailableItemsFromFile(fileName)
+                print("Loaded item definitions from {}".format(fileName))
+                __listItems()
+            except IOError, err:
+                print err
+
+        elif userInput[0] == "/":
+            print "Command invalid"
+
+        else:
+            __im.newTransaction()
+            for c in userInput :
+                try:
+                    __im.addItemToBasket(c)
+                except KeyError:
+                    print 'The item {} doesn\'t exist'.format(c)
+                    return 1
+            print("Ordered Items: ")
+            basketItems = __im.getBasketItems()
+            for i in basketItems:
+                print '{} x {} = {:.2f}'.format(basketItems[i], i.name, basketItems[i] * i.price)
+            while (__im.getBasketValue() > 0):
+                print("Yet to pay:\n{:.2f}".format(__im.getBasketValue()))
+                collected = raw_input("Amount given: ")
+                try:
+                    collected = float(collected)
+                    __im.pay(collected)
+
+                except :
+                    print("Transaction cancelled")
+                    return 1
+            print("Anount to return : {:.2f}".format(-__im.getBasketValue()))
+            __im.saveBasketToHistory()
+            print("Transaction committed\n")
+
+
     while(True):
         try:
             userInput = raw_input("# ");
         except:
             __exitProgram() ## We caught Ctrl + D
+
         __parseInput(userInput)
-
-
-
--- ItemManager.py
+++ ItemManager.py
@@ -7,30 +7,52 @@ import Item
 import pickle
 
 class ItemManager(object):
+    __basketItems = {}
+    __basketValue = 0
+    availableItems = {}
+
 
 
     def __init__(self):
-        self.items = {}
+        pass
 
-    def saveToFile(self, fileName):
+    def saveAvailableItemsToFile(self, fileName):
         f = open(fileName, "w")
-        pickle.dump(self.items, f)
+        pickle.dump(self.availableItems, f)
 
-    def loadFromFile(self, fileName):
+    def loadAvailableItemsFromFile(self, fileName):
         f = open(fileName, "r")
-        self.items = pickle.load(f)
+        self.availableItems = pickle.load(f)
+
+    def addItemToBasket(self, itemIdentifier):
+        i = self.availableItems[itemIdentifier]
+        if i in self.__basketItems:
+                    self.__basketItems[i] = self.__basketItems[i] + 1
+        else:
+                    self.__basketItems[i] = 1
+        self.__basketValue += i.price
 
 
+    def getBasketItems(self):
+        return self.__basketItems
 
+    def getBasketValue(self):
+        return self.__basketValue
 
+    def pay(self, amount):
+        self.__basketValue -= amount
 
+    def createNewItem(self, item):
+        if(item.name in self.availableItems):
+            raise ValueError('Item identifier is already used.')
+        self.availableItems[item.name] = item
 
+    def newTransaction(self):
+            self.__basketItems = {}
+            self.__basketValue = 0
 
-
-
-
-
-
-
-
-
+    def saveBasketToHistory(self):
+        f = open("history.txt", "a")
+        for item in self.__basketItems:
+            f.write('{} * {} @ {} = {}\n'.format(self.__basketItems[item], item.name, item.price, item.price * self.__basketItems[item]))
+        f.close()
